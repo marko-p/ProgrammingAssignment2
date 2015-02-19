0a182beb711859ec764a483b00dc900d5af39571
Marko Praprotnik <marko@praprotnik.si>
2015-02-19 21:55:17 +0100

    Done, added functionality to solve and cache the inverse of a square invertible matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions calculate and cache the inverse
+## of a matrix
 
-## Write a short comment describing this function
-
+## This function caches the inverse matrix and
+## acts as a special vector
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse_matrix <- NULL
+    set <- function(y) {
+        x <<- y
+        inverse_matrix <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) inverse_matrix <<- inverse
+    getinverse <- function() inverse_matrix
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function solves the inverse matrix
+## We assume the given matrix is square and
+## invertible
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    im <- x$getinverse()
+    if(!is.null(im)) {
+        message("getting cached data")
+        return(im)
+    }
+    data <- x$get()
+    im <- solve(data, ...)
+    x$setinverse(im)
+    
+    ## Return a matrix that is the inverse of 'x'
+    im
 }
+
+
+## I tested the functions with the following:
+## > A <- matrix(c(0,1,1,1), nrow = 2, ncol = 2)
+## > iA <- makeCacheMatrix(A)
+## > cacheSolve(iA)
+##       [,1] [,2]
+## [1,]   -1    1
+## [2,]    1    0
+## > cacheSolve(iA)
+## getting cached data
+##       [,1] [,2]
+## [1,]   -1    1
+## [2,]    1    0
